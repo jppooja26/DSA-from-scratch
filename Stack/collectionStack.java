f423e706219002d1789e9157c47afef3ed5ea8f6
Pooja <83561650+jppooja26@users.noreply.github.com>
2023-07-19 13:06:44 +0530

    Add file via upload

---
+++ Stack/collectionStack.java
@@ -0,0 +1,42 @@
+package Stack;
+import java.util.Scanner;
+import java.util.Stack;
+public class collectionStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ch;
+        Stack<Integer> stack = new Stack<>();
+        do {
+            System.out.println("***MENU***");
+            System.out.println("1. Push\n2. Pop\n3. Peek\n4. Exit");
+            System.out.print("Enter your choice: ");
+            ch = sc.nextInt();
+            switch(ch)
+            {
+                case 1:
+                    System.out.print("Enter value: ");
+                    int val = sc.nextInt();
+                    stack.push(val);
+                    System.out.println("Pushed "+ val+" to stack");
+                    break;
+                case 2:
+                    if(!stack.isEmpty())
+                        System.out.println("Popped "+stack.pop()+" from stack");
+                    else
+                        System.out.println("Empty stack");
+                    break;
+                case 3:
+                    if(!stack.isEmpty())
+                        System.out.println("Peek: "+stack.peek());
+                    else
+                        System.out.println("Peek: -1");
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        } while(ch!=-1);
+    }
+}
---
+++ Stack/linkedStack.java
@@ -0,0 +1,68 @@
+package Stack;
+import java.util.Scanner;
+class Node {
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+class StackLinked {
+    Node top;
+    public void push(int value) {
+        Node newNode = new Node(value);
+        newNode.next = top;
+        top = newNode;
+        System.out.println("Pushed "+value+" to stack");
+    }
+    public void pop() {
+        if(isEmpty())
+            System.out.println("Empty stack");
+        else {
+            System.out.println("Popped "+top.data+" from stack");
+            top = top.next;
+        }
+    }
+    public int peek() {
+        if(isEmpty())
+            return -1;
+        else
+            return top.data;
+    }
+    public boolean isEmpty() {
+        return (top==null);
+    }
+}
+public class linkedStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StackLinked obj = new StackLinked();
+        int ch;
+        do {
+            System.out.println("***MENU***");
+            System.out.println("1. Push\n2. Pop\n3. Peek\n4. Exit");
+            System.out.print("Enter your choice: ");
+            ch = sc.nextInt();
+            switch(ch) 
+            {
+                case 1:
+                    System.out.print("Enter value: ");
+                    int val = sc.nextInt();
+                    obj.push(val);
+                    break;
+                case 2:
+                    obj.pop();
+                    break;
+                case 3:
+                    System.out.println("Peek: "+obj.peek());
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        } while(ch!=-1);
+    }
+}
